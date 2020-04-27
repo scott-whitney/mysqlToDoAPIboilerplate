7ba5adbe40ebba3b97c3149797c03cdc830cd391
Minroe <scott.whitney334@gmail.com>
2020-04-27 11:00:28 -0700

    work

--- config/connection.js
+++ config/connection.js
@@ -3,16 +3,14 @@ const mysql = require('mysql2');
 let connection;
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.envJAWSDB_URL).promise();
-}else {
+  connection = mysql.createConnection(process.env.JAWSDB_URL).promise();
+} else {
   connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'password',
     database: 'To_Dos_DB',
-  }).promise();
-  
+}).promise();
   module.exports = connection;
-
-}
+};
 
